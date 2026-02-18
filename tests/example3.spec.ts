00bedad13f4cbc4f33b8b5fd2a5728aac1ea8c4e
Rodrigo Barrios <rodrigoedu11@gmail.com>
2026-02-18 10:08:46 -0500

    Test fail case

--- tests/example3.spec.ts
+++ tests/example3.spec.ts
@@ -15,38 +15,31 @@ const student = {
   hobbies: ["reading"],
 };
 
-test.describe.skip("Practice Form", () => {
-  test.beforeEach("Go to page", async ({ page }) => {
-    await page.goto(URL);
-    await expect(
-      page.getByRole("heading", { name: "Practice Form" }),
-    ).toBeVisible();
-  });
-
-  test("Fill out form", async ({ page }) => {
-    await page.getByPlaceholder("First Name").fill(student.firstName);
-    await page.getByPlaceholder("Last Name").fill(student.lastName);
-    await page.getByPlaceholder("name@example.com").fill(student.email);
-    await page.locator("label", { hasText: student.gender }).click();
-    await page.getByPlaceholder("Mobile number").fill(student.mobile);
-    await page.locator("#dateOfBirthInput").fill(student.dateBirth);
-    await page.locator("#subjectsInput").fill("Parkour");
-    for (const hobbie of student.hobbies) {
-      await page.locator("label", { hasText: hobbie }).click();
-    }
-    await page.getByLabel("Select picture").setInputFiles(student.picture);
-    await page.getByPlaceholder("Current Address").fill(student.address);
-    async function selectReactOption(containerId: string, value: string) {
-      await page.locator(`#${containerId}`).click();
-      await page.keyboard.type(value);
-      await page.keyboard.press("Enter");
-    }
-    await selectReactOption("state", student.state);
-    await selectReactOption("city", student.city);
-    await page.getByRole("button", { name: "Submit" }).click();
-    await expect(
-      page.getByText("Thanks for submitting the form"),
-    ).toBeVisible();
-    await page.getByRole("button", { name: "Close" }).click({ force: true });
-  });
+test.fail("Fill out form", async ({ page }) => {
+  await page.goto(URL);
+  await expect(
+    page.getByRole("heading", { name: "Practice Form" }),
+  ).toBeVisible();
+  await page.getByPlaceholder("First Name").fill(student.firstName);
+  await page.getByPlaceholder("Last Name").fill(student.lastName);
+  await page.getByPlaceholder("name@example.com").fill(student.email);
+  await page.locator("label", { hasText: student.gender }).click();
+  await page.getByPlaceholder("Mobile number").fill(student.mobile);
+  await page.locator("#dateOfBirthInput").fill(student.dateBirth);
+  await page.locator("#subjectsInput").fill("Parkour");
+  for (const hobbie of student.hobbies) {
+    await page.locator("label", { hasText: hobbie }).click();
+  }
+  await page.getByLabel("Select picture").setInputFiles(student.picture);
+  await page.getByPlaceholder("Current Address").fill(student.address);
+  async function selectReactOption(containerId: string, value: string) {
+    await page.locator(`#${containerId}`).click();
+    await page.keyboard.type(value);
+    await page.keyboard.press("Enter");
+  }
+  await selectReactOption("state", student.state);
+  await selectReactOption("city", student.city);
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Thanks for submitting the form")).toBeVisible();
+  await page.getByRole("button", { name: "Close" }).click({ force: true });
 });
